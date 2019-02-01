7db2fb6b740a386076c9f60d7645f439778a6e57
Patrick Rich <patrick@marathonhomecare.com>
2019-01-31 19:05:24 -0500

    Add files via upload

---
+++ hundredyearsold.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+age = int(age)
+year_diff = 100 - age
+year_hundred = 2018 + year_diff
+print("In the year " + str(year_hundred) + " you will be 100 years old, " + name + ".")
